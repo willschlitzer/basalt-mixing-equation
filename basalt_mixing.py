40684bcd1998ff5f77dff25ce7796f2fcabe42df
Will Schlitzer <schlitzer90@gmail.com>
2018-07-19 23:23:13 -0500

    Created mixer() function to simulate mixing between ratios

--- basalt_mixing.py
+++ basalt_mixing.py
@@ -15,3 +15,31 @@ def ratio_ratio_mixing(sample1_data, sample2_data):
     B = a1 * b2 - a2 * b1
     C = a2 * b1 * x1 - a1 * b2 * x2
     D = a1 * b2 * x2 * y1 - a2 * b1 * x1 * y2
+    return x1, y1, x2, y2, A, B, C, D
+
+
+def mixer(sample1_data, sample2_data):
+    """Generates the mixing data based upon the sample"""
+    # The mixing percentage elapsed between calculations
+    step_length = .1
+    x1, y1, x2, y2, A, B, C, D = ratio_ratio_mixing(sample1_data, sample2_data)
+    # Creates the current percentage elapsed variable
+    current_step = 0  # 0% mixed at the beginning
+    # Creates an empty list that will hold the data
+    mixing_data = []
+    while current_step <= 1:
+        # Creates the current x-value based upon mixing percentage
+        x = x1 * current_step + x2 * (1 - current_step)
+        # Determines the y-value based upon x-value
+        y = (-(A * x) - D) / (B * x + C)
+        # Appends current percentage to list
+        mixing_data.append([x, y])
+        # Advances to next mixing percentage
+        current_step += step_length
+    return mixing_data
+
+
+# A demonstration of test data
+sample1_data = [12.73, 15, 12.73, 5.56]
+sample2_data = [1.39, 6, 1.39, 2.06]
+print(mixer(sample1_data, sample2_data))
