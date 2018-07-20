9e41fea7936b0762e217bdd10a8ad5326525d516
Will Schlitzer <schlitzer90@gmail.com>
2018-07-19 22:56:58 -0500

    Run black on basalt_mixing.py

--- basalt_mixing.py
+++ basalt_mixing.py
@@ -1,7 +1,7 @@
 def ratio_ratio_mixing(sample1_data, sample2_data):
     """Creates the variables for 2 element or isotope ratios"""
-    x1 = sample1_data[0]/sample1_data[1]
-    y1 = sample1_data[2]/sample1_data[3]
+    x1 = sample1_data[0] / sample1_data[1]
+    y1 = sample1_data[2] / sample1_data[3]
     x2 = sample2_data[0] / sample2_data[1]
     y2 = sample2_data[2] / sample2_data[3]
     # a1 and a2 are the denominators for the y-axis ratios
@@ -11,7 +11,7 @@ def ratio_ratio_mixing(sample1_data, sample2_data):
     a2 = sample2_data[3]
     b2 = sample2_data[1]
     # A, B, C, D are coefficients for the mixing equation
-    A = a2*b1*y2 - a1*b2*y1
-    B = a1*b2 - a2*b1
-    C = a2*b1*x1 - a1*b2*x2
-    D = a1*b2*x2*y1 - a2*b1*x1*y2
+    A = a2 * b1 * y2 - a1 * b2 * y1
+    B = a1 * b2 - a2 * b1
+    C = a2 * b1 * x1 - a1 * b2 * x2
+    D = a1 * b2 * x2 * y1 - a2 * b1 * x1 * y2
