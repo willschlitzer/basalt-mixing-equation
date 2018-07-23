202f7c3d79e23b6a69237617b27723603224cf24
Will Schlitzer <schlitzer90@gmail.com>
2018-07-22 19:59:16 -0500

    Add ratio_element_mixing()

--- basalt_mixing.py
+++ basalt_mixing.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 
 from sample_data import ratio_ratio_sample1_data, ratio_ratio_sample2_data
 
+
 def ratio_ratio_mixing(sample1_data, sample2_data):
     """Creates the variables for 2 element or isotope ratios"""
     x1 = sample1_data[0] / sample1_data[1]
@@ -21,6 +22,24 @@ def ratio_ratio_mixing(sample1_data, sample2_data):
     B = a1 * b2 - a2 * b1
     C = a2 * b1 * x1 - a1 * b2 * x2
     D = a1 * b2 * x2 * y1 - a2 * b1 * x1 * y2
+    return x1, y1, x2, y2, A, B, C, D
+
+
+def ratio_element_mixing(sample1_data, sample2_data):
+    """Creates the variables for a ratio/ratio and element"""
+    x1 = sample1_data[0]
+    y1 = sample1_data[1] / sample2_data[2]
+    x2 = sample2_data[0]
+    y2 = sample2_data[1] / sample3_data[2]
+    # b=1 since the denominators for the x-axis are 1
+    # a1 and a2 are the denominators for the y-axis ratios
+    a1 = sample1_data[2]
+    a2 = sample2_data[2]
+    # A, B, C, D are coefficients for the mixing equation
+    A = a2 * y2 - a1 * y1
+    B = a1 - a2
+    C = a2 * x1 - a1 * x2
+    D = a1 * x2 * y1 - a2 * x1 * y2
     return x1, y1, x2, y2, A, B, C, D
 
 
@@ -43,13 +62,12 @@ def mixer(sample1_data, sample2_data):
         # Advances to next mixing percentage
         current_step += step_length
     mixing_data_array = np.array(mixing_data)
-    mixing_data_df = pd.DataFrame(mixing_data_array, columns=['current_step', 'x', 'y'])
+    mixing_data_df = pd.DataFrame(mixing_data_array, columns=["current_step", "x", "y"])
     return mixing_data_df
 
 
 # A demonstration of test data
 df = mixer(ratio_ratio_sample1_data, ratio_ratio_sample2_data)
-df.plot('x', 'y')
+df.plot("x", "y")
 print(df)
 plt.show()
-
