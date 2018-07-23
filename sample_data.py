13932ec990d9462cbcc742e0fb1575516a59b8ba
Will Schlitzer <schlitzer90@gmail.com>
2018-07-22 20:15:32 -0500

    Add ratio_element and element_element data to sample_data.py

--- sample_data.py
+++ sample_data.py
@@ -1,5 +1,21 @@
-# Data from FLAMINGO MV1007 data
+# data from FLAMINGO MV1007 data
 # sample1 data from D01A
 # sample2 data from D04A
+
+# data for ratio-ratio mixing
+# x-axis La/Nb
+# y-axis La/Sm
 ratio_ratio_sample1_data = [12.94, 18.91, 12.94, 5.73]
 ratio_ratio_sample2_data = [1.56, 1.08, 1.56, 2.36]
+
+# data for ratio-element mixing
+# x-axis La
+# y-axis La/Sm
+ratio_element_sample1_data = [12.94, 12.94, 5.73]
+ratio_element_sample2_data = [1.56, 1.56, 2.35]
+
+# data for element-element mixing
+# x-axis La
+# y-axis Sm
+element_element_sample1_data = [12.94, 5.73]
+element_element_sample2_data = [1.56, 2.35]
