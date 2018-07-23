94d220a6091f451456fd0cf2b0c7a76a55aa8565
Will Schlitzer <schlitzer90@gmail.com>
2018-07-22 20:09:44 -0500

    Add block comments for data source for sample_data.py

--- sample_data.py
+++ sample_data.py
@@ -1,2 +1,5 @@
-ratio_ratio_sample1_data = [13.79, 1, 13.79, 19.38]
-ratio_ratio_sample2_data = [1.39, 1, 1.39, 2.06]
+# Data from FLAMINGO MV1007 data
+# sample1 data from D01A
+# sample2 data from D04A
+ratio_ratio_sample1_data = [12.94, 18.91, 12.94, 5.73]
+ratio_ratio_sample2_data = [1.56, 1.08, 1.56, 2.36]
